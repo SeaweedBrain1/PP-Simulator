13082c70c733a0bc02487b839f38cb15a4f12b22
SeaweedBrain1 <oskar.marzec1234@gmail.com>
2024-10-27 18:18:02 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,22 @@
+﻿namespace Simulator;
+
+internal class Creature
+{
+    private string? name;
+    public string? Name { get; set; }
+
+    private int level;
+    public int Level {  get; set; }
+
+    public string Info => $"{Name} - [{Level}]";
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+
+    public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+}
