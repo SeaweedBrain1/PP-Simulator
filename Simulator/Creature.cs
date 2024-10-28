ec5d4f3497053054d0a45b5337db2ba89c451613
SeaweedBrain1 <oskar.marzec1234@gmail.com>
2024-10-28 20:46:57 +0100

    Settery

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,11 +2,42 @@
 
 internal class Creature
 {
-    private string? name;
-    public string? Name { get; set; }
+    private string name = "Unknown";
+    public string Name
+    {
+        get => name;
+        init
+        {
+            value = value.Trim();
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }
+            else if (value.Length > 25)
+            {
+                value = value.Substring(0, 25).TrimEnd();
+                if (value.Length < 3)
+                {
+                    value = value.PadRight(3, '#');
+                }
+            }
+            if (char.IsLower(value[0]))
+            {
+                value = char.ToUpper(value[0]) + value.Substring(1);
+            }
 
-    private int level;
-    public int Level {  get; set; }
+            name = value;
+        }
+    }
+    private int level = 1;
+    public int Level 
+    {
+        get => level;
+        init
+        {
+            level = value < 1 ? 1 : (value > 10 ? 10 : value);
+        }
+    }
 
     public string Info => $"{Name} - [{Level}]";
 
@@ -19,4 +50,12 @@ internal class Creature
     public Creature() { }
 
     public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+
+    public void Upgrade()
+    {
+        if (level < 10)
+        {
+            level++;
+        }
+    }
 }
