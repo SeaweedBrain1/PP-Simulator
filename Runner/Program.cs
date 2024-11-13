642a90d9b3571beb745f24ca79b3de38d3fa2e0b
SeaweedBrain1 <oskar.marzec1234@gmail.com>
2024-11-13 14:16:38 +0100

    xUnit

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -13,42 +13,42 @@ internal class Program
         //Lab4b();
         //lab5a();
         //Lab5b();
-        Orc o = new Orc();
-        Console.WriteLine(Orc.Go("UDL"));
+        //Orc o = new Orc();
+        //Console.WriteLine(Orc.Go("UDL"));
     }
 
-    static void Lab4a()
-    {
-        Console.WriteLine("HUNT TEST\n");
-        var o = new Orc() { Name = "Gorbag", Rage = 7 };
-        o.SayHi();
-        for (int i = 0; i < 10; i++)
-        {
-            o.Hunt();
-            o.SayHi();
-        }
+    //static void Lab4a()
+    //{
+    //    Console.WriteLine("HUNT TEST\n");
+    //    var o = new Orc() { Name = "Gorbag", Rage = 7 };
+    //    o.SayHi();
+    //    for (int i = 0; i < 10; i++)
+    //    {
+    //        o.Hunt();
+    //        o.SayHi();
+    //    }
 
-        Console.WriteLine("\nSING TEST\n");
-        var e = new Elf("Legolas", agility: 2);
-        e.SayHi();
-        for (int i = 0; i < 10; i++)
-        {
-            e.Sing();
-            e.SayHi();
-        }
+    //    Console.WriteLine("\nSING TEST\n");
+    //    var e = new Elf("Legolas", agility: 2);
+    //    e.SayHi();
+    //    for (int i = 0; i < 10; i++)
+    //    {
+    //        e.Sing();
+    //        e.SayHi();
+    //    }
 
-        Console.WriteLine("\nPOWER TEST\n");
-        Creature[] creatures = {
-        o,
-        e,
-        new Orc("Morgash", 3, 8),
-        new Elf("Elandor", 5, 3)
-    };
-        foreach (Creature creature in creatures)
-        {
-            Console.WriteLine($"{creature.Name,-15}: {creature.Power}");
-        }
-    }
+    //    Console.WriteLine("\nPOWER TEST\n");
+    //    Creature[] creatures = {
+    //    o,
+    //    e,
+    //    new Orc("Morgash", 3, 8),
+    //    new Elf("Elandor", 5, 3)
+    //};
+    //    foreach (Creature creature in creatures)
+    //    {
+    //        Console.WriteLine($"{creature.Name,-15}: {creature.Power}");
+    //    }
+    //}
 
     static void Lab4b()
     {
